52c82ecb69207419084d89d5f7af55db15e11e0b
Станислав <TMWCnf@gmail.com>
2017-09-22 21:11:42 +0300

    finish counter

--- script.js
+++ script.js
@@ -11,29 +11,33 @@ var timer = [0,0,0,0];
 
 // Run a standard minute/second/hundredths timer:
 function runTimer() {
-  let currentTime = time[0] + ":" + timer[1] + ":" + timer[2];
-  theTimer.inner HTML = currentTime;
-  timer[3]++;
+    let currentTime = timer[0] + ":" + timer[1] + ":" + timer[2];
+    theTimer.innerHTML = currentTime;
+    timer[3]++;
+
+    timer[0] = Math.floor((timer[3] / 100) / 60);
+    timer[1] = Math.floor((timer[3] / 100) - (timer[0] * 60));
+    timer[2] = Math.floor(timer[3] - (timer[1] * 100) - (timer[0] * 6000));
 }
 
 // Match the text entered with the provided text on the page:
 function spellCheck() {
-  let textEntered = testArea.value;
-  console.log(textEntered);
+    let textEntered = testArea.value;
+    console.log(textEntered);
 }
 
 // Start the timer:
 function start() {
-  let textEnterdLenght = testArea.value.length;
-  if (textEnterdLenght === 0) {
-    setInterval(runTimer, 10);
-  }
-  console.log(textEnterdLenght);
+    let textEnterdLenght = testArea.value.length;
+    if (textEnterdLenght === 0) {
+      setInterval(runTimer, 10);
+    }
+    console.log(textEnterdLenght);
 }
 
 // Reset everything:
 function reset() {
-  console.log("reset button has been pressed!");
+    console.log("reset button has been pressed!");
 }
 
 // Event listeners for keyboard input and the reset button:
