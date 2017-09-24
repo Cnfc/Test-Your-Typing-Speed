599e86c9c264951e3a9cc0c8dac2bac207570c8e
Станислав <TMWCnf@gmail.com>
2017-09-24 21:30:26 +0300

    finish work part

--- script.js
+++ script.js
@@ -57,8 +57,15 @@ function start() {
 
 // Reset everything:
 function reset() {
-    console.log("reset button has been pressed!");
-}
+    clearInterval(interval);
+    interval = null;
+    timer = [0, 0, 0, 0];
+    timeRunning = false;
+
+    testArea.value = "";
+    theTimer.innerHTML = "00:00:00";
+    testWrapper.style.borderColor = "grey";
+ }
 
 // Event listeners for keyboard input and the reset button:
 testArea.addEventListener("keypress", start, false);
